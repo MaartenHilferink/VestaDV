858d43613ea4482ce85068629b42cc85bb5d032f
jipclaassens <Kakatu11>
2020-10-28 16:28:59 +0100

    Inlezen warmtebronnen generieker 2050 stap toegevoegd ipv alles dubbel te doen Warmtebronnen worden nu allemaal weggeschreven, afhankelijk van strategie Regioselectie in export eruit, wordt bepaald door StudyArea in sharedinvoer

--- Runs/path/set.bat
+++ Runs/path/set.bat
@@ -1,7 +1,7 @@
 REM aanpassingen voor machine specifieke paden
 set prj_dir=C:\GeoDMS\ProjDir\VestaDV
 set log_dir=C:\GeoDMS\ProjDir\VestaDV\log_vesta
-set exe_dir=C:\Program Files\ObjectVision\GeoDms7232
+set exe_dir=C:\Program Files\ObjectVision\GeoDms7233
 
 
 set exe_path=%exe_dir%\GeoDMSRun.exe
