ec01f6c63e8f2d55ded4ddf82b1211bf6f7a1916
jipclaassens <Kakatu11>
2020-11-26 09:48:58 +0100

    s0 werkend gemaakt

--- Runs/batch_leidraad_ESDL.bat
+++ Runs/batch_leidraad_ESDL.bat
@@ -25,12 +25,12 @@ REM "%exe_path%" "%prj_dir%\Runs\S0_Referentie.dms" "%EXPORT_PATH%"
 REM "%exe_path%" "%prj_dir%\Runs\S1a_B_LuchtWP.dms" "%EXPORT_PATH%"
 REM "%exe_path%" "%prj_dir%\Runs\S1b_B_BodemWP.dms" "%EXPORT_PATH%"
 
-REM  "%exe_path%" "%prj_dir%\Runs\S2a_B_Restwarmte.dms" "%EXPORT_PATH%"
-REM  "%exe_path%" "%prj_dir%\Runs\S2b_B_Geo_contour.dms" "%EXPORT_PATH%"
-REM  "%exe_path%" "%prj_dir%\Runs\S2c_B_Geo_overal.dms" "%EXPORT_PATH%"
-REM  "%exe_path%" "%prj_dir%\Runs\S2d_D_Restwarmte.dms" "%EXPORT_PATH%"
-REM  "%exe_path%" "%prj_dir%\Runs\S2e_D_Geo_contour.dms" "%EXPORT_PATH%" REM Gaat niet door
-REM  "%exe_path%" "%prj_dir%\Runs\S2f_D_Geo_overal.dms" "%EXPORT_PATH%" REM Gaat niet door
+REM "%exe_path%" "%prj_dir%\Runs\S2a_B_Restwarmte.dms" "%EXPORT_PATH%"
+REM "%exe_path%" "%prj_dir%\Runs\S2b_B_Geo_contour.dms" "%EXPORT_PATH%"
+REM "%exe_path%" "%prj_dir%\Runs\S2c_B_Geo_overal.dms" "%EXPORT_PATH%"
+REM "%exe_path%" "%prj_dir%\Runs\S2d_D_Restwarmte.dms" "%EXPORT_PATH%"
+REM "%exe_path%" "%prj_dir%\Runs\S2e_D_Geo_contour.dms" "%EXPORT_PATH%" 
+REM "%exe_path%" "%prj_dir%\Runs\S2f_D_Geo_overal.dms" "%EXPORT_PATH%" 
 
 REM "%exe_path%" "%prj_dir%\Runs\S3a_B_LT30_30.dms" "%EXPORT_PATH%"
 REM "%exe_path%" "%prj_dir%\Runs\S3b_B_LT30_70.dms" "%EXPORT_PATH%"
@@ -42,12 +42,12 @@ REM "%exe_path%" "%prj_dir%\Runs\S3g_D_BuurtWKO.dms" "%EXPORT_PATH%"
 REM "%exe_path%" "%prj_dir%\Runs\S3h_D_TEO.dms" "%EXPORT_PATH%"
 
 REM  "%exe_path%" "%prj_dir%\Runs\S4a_GG_B_hWP.dms" "%EXPORT_PATH%"
-REM  "%exe_path%" "%prj_dir%\Runs\S4b_GG_B_HR.dms" "%EXPORT_PATH%" REM Gaat niet door
-  "%exe_path%" "%prj_dir%\Runs\S4c_GG_D_hWP.dms" "%EXPORT_PATH%"
-  "%exe_path%" "%prj_dir%\Runs\S4d_GG_D_HR.dms" "%EXPORT_PATH%"
+REM  "%exe_path%" "%prj_dir%\Runs\S4b_GG_B_HR.dms" "%EXPORT_PATH%" 
+REM  "%exe_path%" "%prj_dir%\Runs\S4c_GG_D_hWP.dms" "%EXPORT_PATH%"
+REM  "%exe_path%" "%prj_dir%\Runs\S4d_GG_D_HR.dms" "%EXPORT_PATH%"
 
-  "%exe_path%" "%prj_dir%\Runs\S5a_H2_B_hWP.dms" "%EXPORT_PATH%"
-  "%exe_path%" "%prj_dir%\Runs\S5b_H2_B_HR.dms" "%EXPORT_PATH%"
+ REM "%exe_path%" "%prj_dir%\Runs\S5a_H2_B_hWP.dms" "%EXPORT_PATH%"
+REM  "%exe_path%" "%prj_dir%\Runs\S5b_H2_B_HR.dms" "%EXPORT_PATH%"
  "%exe_path%" "%prj_dir%\Runs\S5c_H2_D_hWP.dms" "%EXPORT_PATH%"
  "%exe_path%" "%prj_dir%\Runs\S5d_H2_D_HR.dms" "%EXPORT_PATH%"
 
